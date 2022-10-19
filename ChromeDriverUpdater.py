830314fddbbdd78989ddac084bf480f1eb1028a9
Paolo Marconi <paolo.marconi@DufenergyTrading.onmicrosoft.com>
2022-10-19 14:48:58 +0200

    Autochromedriverupdater corrected, first working version. Folders name should be fixed

--- ChromeDriverUpdater.py
+++ ChromeDriverUpdater.py
@@ -1,30 +1,25 @@
-import wget
 import requests
-import xml.etree.ElementTree as ET
-from xml.dom import minidom
+from zipfile import ZipFile
 
+BASE_ZIP_PATH = 'https://chromedriver.storage.googleapis.com/'
+ZIP_FILE_NAME = 'chromedriver_win32.zip'
+ZIP_LOCAL_FOLDER = 'C:/Users/paolo.marconi/Desktop/pha/'
 
-# url='https://golang.org/dl/go1.17.3.windows-amd64.zip'
-# get.download(url)
-# https://chromedriver.storage.googleapis.com/index.html?path=100.0.4896.20/ with final version ?
-# zip_name = chromedriver_win32.zip
 
 def start_update():
-    response = requests.get("https://chromedriver.storage.googleapis.com/")
-    text_to_parse = response.text
-    version_string = parse_response2(text_to_parse)
+    response = requests.get("https://chromedriver.storage.googleapis.com/LATEST_RELEASE")
+    zip_url = BASE_ZIP_PATH + response.text + '/' + ZIP_FILE_NAME
+    req = requests.get(zip_url)
+    print(zip_url)
+    with open(get_zip_name(response.text), 'wb') as output_file:
+        output_file.write(req.content)
+    zf = ZipFile(get_zip_name(response.text), 'r')
+    zf.extractall(ZIP_LOCAL_FOLDER)
+    zf.close()
 
 
-def get_version_number(st):
-
-
-
-def parse_response2(text):
-    p3 = minidom.parseString(text)
-    tagnames = p3.getElementsByTagName('Key')
-    keys = []
-    for tag in tagnames:
-        keys.append(get_version_number(tag.firstChild.data))
+def get_zip_name(version):
+    return ZIP_LOCAL_FOLDER + 'chromedriver_' + version + '.zip'
 
 
 start_update()
