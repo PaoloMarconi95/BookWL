b5abdb8d44ae65ac122848cb57e2eb7cd8dccd1b
Paolo Marconi <paolomarconi1995@gmail.com>
2023-07-12 09:00:51 +0200

    Safe Access logging improved

--- Tasks/SafeAccess.py
+++ Tasks/SafeAccess.py
@@ -2,8 +2,14 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+import traceback
+
 # Custom
 import Log
+from SendEmail import send_email
+
 log = Log.logger
 
 def safe_access_by_id(driver, id_string, max_attempts=5):
@@ -14,11 +20,16 @@ def safe_access_by_id(driver, id_string, max_attempts=5):
 
     attempts = 0
     while not EC.staleness_of(element) and attempts < max_attempts:
-        log.warn('stale element found, trying to re-define it')
-        element = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.ID, id_string))
-        )
-        attempts += 1
+        try:
+            log.warn('stale element found, trying to re-define it')
+            element = WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.ID, id_string))
+            )
+        except (NoSuchElementException, StaleElementReferenceException) as e:
+            traceback.print_exc()
+            send_email("paolomarconi1995@gmail.com", "Eccezione Verificata! Controlla Il log", str(e))
+        finally:
+            attempts += 1
 
     if attempts < max_attempts:
         return element
