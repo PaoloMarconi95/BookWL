be0d4e0bbd53bf7dacde8419e9d45d3362a210dd
PaoloMarconi95 <paolomarconi1995@gmail.com>
2023-11-22 17:31:01 +0100

    Chromedriver updater deleted, useless for raspi instance

--- Tasks/WebDriverFactory.py
+++ Tasks/WebDriverFactory.py
@@ -1,31 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import SessionNotCreatedException, WebDriverException
-import subprocess
 
 # Custom
 from Exceptions import DriverNotInitializedException
-from Tasks.ChromeDriverUpdater import update_chromedriver, get_latest_chromedriver_version
-from Config import CONFIG, LOGGER
+from Config import LOGGER
 import os
 
 
 class WebDriverFactory:
-
-    def __init__(self):
-        LOGGER.info('New instance of WebDriverFactory requested, checking if chromedriver is updated...')
-        if not self.__is_driver_updated():
-            update_chromedriver()
-
-
-    def __is_driver_updated(self):
-        last_verison = get_latest_chromedriver_version()
-        check_installed_version_command = f"cd {CONFIG.chromedriver_folder} | chromedriver -v"
-        installed_version = subprocess.check_output(check_installed_version_command, shell=True).decode()
-        if last_verison in installed_version:
-            return True
-        else:
-            return False
 
     def get_driver(self):
         options = Options()
